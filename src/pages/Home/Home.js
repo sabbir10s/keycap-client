4c549518a0653cc3848479c938ce8587a1d1f15b
sabbir10s <sabbirahmed1023@gmail.com>
2022-05-25 01:04:25 +0600

    newsletter section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../../components/Footer';
 import Banner from './Banner/Banner';
 import Download from './Download/Download';
+import Newsletter from './Newsletter/Newsletter';
 import Products from './Products/Products';
 import Reviews from './Reviews/Reviews';
 import Summary from './Summary/Summary';
@@ -14,6 +15,7 @@ const Home = () => {
             <Products />
             <Reviews />
             <Download />
+            <Newsletter />
             <Footer />
         </div>
     );
---
+++ src/pages/Home/Newsletter/Newsletter.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Newsletter = () => {
+    return (
+        <div className='bg-primary py-10'>
+            <h1 className='text-3xl text-center text-base-100'>NEWSLETTER</h1>
+
+            <div className='flex justify-center'>
+                <div class="card w-full max-w-lg">
+                    <div class="card-body">
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text text-base-100">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" class="input input-bordered" />
+                        </div>
+                        <div className='flex gap-3 w-full'>
+                            <div class="form-control w-full">
+                                <label class="label">
+                                    <span class="label-text text-base-100">First Name</span>
+                                </label>
+                                <input type="text" placeholder="First Name" class="input input-bordered w-full" />
+                            </div>
+
+                            <div class="form-control w-full">
+                                <label class="label">
+                                    <span class="label-text text-base-100">Last Name</span>
+                                </label>
+                                <input type="text" placeholder="Last Name" class="input input-bordered w-full" />
+                            </div>
+                        </div>
+                        <p className='text-base-100 max-10 mt-5'>
+                            By providing your email address and cliking on "submit", you accept to receive by email our updates about Irimo products and SNA EUROPE other innovations and events.
+                        </p>
+                        <div class="form-control mt-6">
+                            <button class="btn btn-outline border-base-100 text-base-100">submit</button>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Newsletter;
