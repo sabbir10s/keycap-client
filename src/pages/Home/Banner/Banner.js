51c2ffc2adea1536b9a132d8e416ca1b2ba3c59b
sabbir10s <sabbirahmed1023@gmail.com>
2022-09-18 13:05:25 +0600

    product cart design updateded

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -2,8 +2,8 @@ import React from 'react';
 import storage from '../../../images/storage.png'
 const Banner = () => {
     return (
-        <div className="hero bg-base-100 py-10">
-            <div className="hero-content  flex-col lg:flex-row-reverse justify-between gap-10">
+        <div className="hero min-h-screen bg-base-100">
+            <div className="hero-content  flex-col lg:flex-row-reverse justify-between md:mx-5 lg:mx-10 gap-10">
                 <div className='lg:w-50%'>
                     <img src={storage} className="rounded-lg w-[450px]" alt='' />
                 </div>
@@ -12,7 +12,7 @@ const Banner = () => {
                         <h1 className="text-4xl md:text-5xl font-bold">TOOL STORAGE!</h1>
                         <p className="py-6">NEXIQ tool storage system allows professionals to easy
                             access the tools and becomes a relevant time saver in crucial activities.</p>
-                        <button className="btn btn-primary"><a href="#products">BOOK NOW</a></button>
+                        <button className="btn btn-primary">BOOK NOW</button>
                     </div>
                 </div>
             </div>
--- src/pages/Home/Products/ProductCard.js
+++ src/pages/Home/Products/ProductCard.js
@@ -8,17 +8,17 @@ const ProductCard = ({ product }) => {
         navigate(`/purchase/${_id}`)
     }
     return (
-        <div className='border border-primary hover:shadow-xl hover:bg-neutral flex flex-col justify-between'>
+        <div className='border border-primary rounded-lg shadow-lg shadow-base-300/60 flex flex-col justify-between duration-150 hover:scale-110 cursor-pointer'>
             <div className='flex justify-center items-center'>
-                <img className=' px-5 pt-5 h-[180px]' src={image} alt="" />
+                <img className=' px-5 pt-5 h-[130px]' src={image} alt="" />
             </div>
-            <div className='p-5'>
+            <div className='p-3 my-3'>
 
-                <p className='text-2xl font-bold'>{name}</p>
-                <p><small>{description.slice(0, 100)}</small></p>
-                <div className='text-xl mt-1'>Price: <span className='text-primary font-bold'>${price}</span></div>
+                <p className='text-lg font-bold'>{name}</p>
+                {/* <p><small>{description.slice(0, 100)}</small></p> */}
+                <div className='text-xl mt-1'><span className='text-primary font-bold'>${price}</span></div>
             </div>
-            <button onClick={() => handlePurchase(_id)} className='w-full btn btn-primary border border-primary rounded-none'>Book Now</button>
+            <button onClick={() => handlePurchase(_id)} className='w-full bg-primary text-base-100 py-1 rounded-b-lg'>Book Now</button>
         </div>
     );
 };
