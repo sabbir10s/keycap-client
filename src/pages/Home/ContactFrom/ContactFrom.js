cfc4ea29a998679393134435f2dc301c9baa1d4d
sabbir10s <sabbirahmed1023@gmail.com>
2023-09-08 13:23:35 +0600

    contact form updated

--- src/pages/Home/ContactFrom/ContactFrom.js
+++ src/pages/Home/ContactFrom/ContactFrom.js
@@ -14,42 +14,42 @@ const ContactFrom = () => {
     return (
         <div className='bgImage'>
             <div className='bg-primary-700/90 py-10 md:py-20'>
-                <h1 className='text-2xl text-center text-base-100 uppercase md:py-4'>Contact With Us</h1>
+                <h1 className='text-4xl text-center text-gray-100 md:pb-6'>Contact with us</h1>
 
                 <div className='flex justify-center'>
-                    <div className="card w-full max-w-lg">
-                        <div className="card-body">
-                            <form onSubmit={handleMessageSubmit}>
+                    <div className="w-full max-w-lg">
+                        <div>
+                            <form className=' space-y-4' onSubmit={handleMessageSubmit}>
                                 <div className='flex flex-col lg:flex-row gap-3 w-full'>
-                                    <div className="form-control w-full">
+                                    <div className="w-full flex flex-col">
                                         <label >
-                                            <span className="label-text text-base-100">Name</span>
+                                            <span className="text-gray-100 text-SM ">Name</span>
                                         </label>
                                         <input type="text" name='name' placeholder="Name" required autoComplete='off' className="border border-primary-700 p-[10px] rounded-md focus:outline-secondary" />
                                     </div>
 
-                                    <div className="form-control w-full">
+                                    <div className="w-full flex flex-col">
                                         <label >
-                                            <span className="label-text text-base-100">Subject</span>
+                                            <span className="text-gray-100 text-SM ">Subject</span>
                                         </label>
                                         <input type="text" name='subject' placeholder="Subject" required autoComplete='off' className="border border-primary-700 p-[10px] rounded-md focus:outline-secondary" />
                                     </div>
                                 </div>
-                                <div className="form-control">
+                                <div className="w-full flex flex-col">
                                     <label >
-                                        <span className="label-text text-base-100">Email</span>
+                                        <span className="text-gray-100 text-SM ">Email</span>
                                     </label>
                                     <input type="email" name='email' placeholder="email" required autoComplete='off' className="border border-primary-700 p-[10px] rounded-md focus:outline-secondary" />
                                 </div>
 
-                                <div className="form-control">
+                                <div className="w-full flex flex-col">
                                     <label >
-                                        <span className="label-text text-base-100">Message</span>
+                                        <span className="text-gray-100 text-SM ">Message</span>
                                     </label>
                                     <textarea name="message" required id="" cols="30" rows="4" placeholder='Message' autoComplete='off' className="border border-primary-700 p-[10px] rounded-md focus:outline-secondary" ></textarea>
                                 </div>
-                                <div className="form-control mt-5">
-                                    <button className="bg-secondary-500 hover:bg-orange-500 duration-300 shadow-lg uppercase text-lg py-[10px] text-black rounded-md">submit</button>
+                                <div className=" mt-5">
+                                    <button className="text-white bg-secondary-500 hover:bg-orange-600 duration-300 shadow-lg uppercase text-lg py-[10px] rounded-lg w-full">submit</button>
                                 </div>
 
                             </form>
