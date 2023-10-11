7a6266ba9ab895ca38ae715bae3b7a78b0f0f075
sabbir10s <sabbirahmed1023@gmail.com>
2023-10-11 09:42:41 +0600

    product details modal and toltip button done

--- src/pages/Dashboard/Admin/MangeProducts/ProductRow.jsx
+++ src/pages/Dashboard/Admin/MangeProducts/ProductRow.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import Modal from "../../../../shared/Modal";
+import DeleteModal from "../../../../shared/DeleteModal";
+import ProductQuickDetails from "../../../../components/ProductQuickDetails";
 
 const ProductRow = ({ setProducts, product, index, setIsReload, reload }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDeleteModal, setDeleteModal] = useState(false);
 
   const toggle = (productIdToToggle) => {
     setProducts((prevState) =>
@@ -42,6 +45,13 @@ const ProductRow = ({ setProducts, product, index, setIsReload, reload }) => {
 
   const closeModal = () => {
     setIsOpen(false);
+  };
+  const openDeleteModal = () => {
+    setDeleteModal(true);
+  };
+
+  const closeDeleteModal = () => {
+    setDeleteModal(false);
   };
 
   return (
@@ -96,38 +106,40 @@ const ProductRow = ({ setProducts, product, index, setIsReload, reload }) => {
         </button>
       </td>
       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium flex gap-4">
-        <div className="relative flex flex-col items-center group">
-          <button className="text-gray-400 text-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
-          </button>
-          <div className="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
-            <span className="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-success-500 shadow-lg">
-              View
-            </span>
-            <div className="w-3 h-3 -mt-2 rotate-45 bg-success-500"></div>
-          </div>
-        </div>
         <button
           onClick={openModal}
-          className="text-gray-400 text-lg hover:text-red-500 duration-200"
+          className="relative flex flex-col items-center group text-gray-400 text-lg"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          <div class="absolute bottom-0 flex flex-col items-center hidden mb-6 group-hover:flex">
+            <span class="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">
+              Details
+            </span>
+            <div class="w-3 h-3 -mt-2 rotate-45 bg-black"></div>
+          </div>
+        </button>
+
+        <button
+          onClick={openDeleteModal}
+          className="relative flex flex-col items-center group text-gray-400 text-lg hover:text-red-500 duration-200"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -143,10 +155,20 @@ const ProductRow = ({ setProducts, product, index, setIsReload, reload }) => {
               d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
             />
           </svg>
+          <div class="absolute bottom-0 flex flex-col items-center hidden mb-6 group-hover:flex">
+            <span class="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-red-500 shadow-lg">
+              Delete
+            </span>
+            <div class="w-3 h-3 -mt-2 rotate-45 bg-red-500"></div>
+          </div>
         </button>
       </td>
 
       <Modal isOpen={isOpen} onClose={closeModal}>
+        <ProductQuickDetails product={product} />
+      </Modal>
+
+      <DeleteModal isDeleteModal={isDeleteModal} onClose={closeDeleteModal}>
         <div className="max-w-[500px] py-2 text-left">
           <h3 className="text-lg font-semibold">
             Are you sure you wanna delete this product?
@@ -171,7 +193,7 @@ const ProductRow = ({ setProducts, product, index, setIsReload, reload }) => {
             </button>
           </div>
         </div>
-      </Modal>
+      </DeleteModal>
     </tr>
   );
 };
---
+++ src/shared/DeleteModal.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+
+const DeleteModal = ({ children, isDeleteModal, onClose }) => {
+  const handleCloseDeleteModal = (e) => {
+    if (e.target.id === "deleteModalContainer") onClose();
+  };
+
+  useEffect(() => {
+    if (isDeleteModal) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isDeleteModal]);
+
+  if (!isDeleteModal) {
+    return null;
+  }
+
+  return (
+    <button className="cursor-default" onClick={handleCloseDeleteModal}>
+      <div>
+        {isDeleteModal && (
+          <div className="fixed flex items-center justify-center inset-0 z-50">
+            <div
+              id="deleteModalContainer"
+              className="absolute inset-0 bg-black opacity-50"
+            ></div>
+            <div className="bg-white rounded-lg dark:bg-gray-800 p-4 lg:p-8 h-screen md:h-auto max-w-sm md:max-w-2xl lg:max-w-5xl z-10 overflow-auto relative">
+              <button
+                className="absolute right-0 top-0 text-gray-600 dark:text-gray-300 p-4 z-50"
+                onClick={onClose}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+              <div>{children}</div>
+            </div>
+          </div>
+        )}
+      </div>
+    </button>
+  );
+};
+
+export default DeleteModal;
